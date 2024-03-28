7c08a8dd859a419ca3aa27b9cbbbc7e3a9d7dd55
NanuIjaz <122975171+NanuIjaz@users.noreply.github.com>
2024-03-28 13:20:15 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-nix run .#run-cardano-node
-sleep 50
+/root/.nix-profile/bin/nohup bash -c "/root/.nix-profile/bin/nix run .#run-cardano-node 2>&1 &" && sleep 6
+sleep 100
 #nohup bash -c "nix develop .# --command just run-era"
